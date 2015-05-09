3d043a015fc6e7e523a9cd763715a47fb68fd352
Johnson Earls <darkfoxprime@users.noreply.github.com>
2015-05-09 09:09:06 -0700

    Create behavioral tests for "behave".  Initial tests include each node of the AST tree tested individually, as well as some operator precedence testing.

---
+++ tests/steps/format_test.py
@@ -0,0 +1,22 @@
+from behave import *
+from ASTFormatter import ASTFormatter
+import ast
+
+"""
+        Given I have parsed an AST tree from "<source input>",
+         when I transform the AST tree to source,
+         then the output should include "<output snippet>".
+"""
+
+@given("I have parsed an AST tree from \"{source}\"")
+def given_a_source_input_of(context, source):
+    context.tree = ast.parse(source) 
+
+@when("I transform the AST tree to source")
+def when_I_transform_the_tree_to_source(context):
+    context.formatted = ASTFormatter().format(context.tree)
+
+@then("the output should include \"{output}\"")
+def then_the_output_should_include(context, output):
+    if output not in context.formatted:
+        fail("%r not in %r" % (output, context.formatted))
