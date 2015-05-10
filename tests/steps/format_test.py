30c78f8fa0f9db75f43141c1a1295d715a7d28d8
Johnson Earls <darkfoxprime@gmail.com>
2015-05-09 22:16:16 -0700

    try different method of decoding string escapes for python 3

--- tests/steps/format_test.py
+++ tests/steps/format_test.py
@@ -1,6 +1,7 @@
 from behave import *
 from ASTFormatter import ASTFormatter
 import ast
+import sys
 
 """
         Given I have parsed an AST tree from "<source input>",
@@ -8,9 +9,17 @@ import ast
          then the output should include "<output snippet>".
 """
 
+try:
+  "".decode
+  def decode_escapes(s):
+    return s.decode('string_escape')
+except AttributeError:
+  def decode_escapes(s):
+    return bytes(s).decode('string_escape')
+
 @given("I have parsed an AST tree from \"{source}\",")
 def given_a_source_input_of(context, source):
-    context.tree = ast.parse(source.decode('string_escape')) 
+    context.tree = ast.parse(decode_escapes(source)) 
 
 @when("I transform the AST tree to source,")
 def when_I_transform_the_tree_to_source(context):
@@ -18,4 +27,4 @@ def when_I_transform_the_tree_to_source(context):
 
 @then("the output should include \"{output}\".")
 def then_the_output_should_include(context, output):
-    assert output.decode('string_escape') in context.formatted, ("%r not in %r" % (output, context.formatted))
+    assert decode_escapes(output) in context.formatted, ("%r not in %r" % (output, context.formatted))
