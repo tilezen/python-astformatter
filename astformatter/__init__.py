8998d87e331b0f4d4178a2d36a4ddf976c839b2b
Johnson Earls <darkfoxprime@gmail.com>
2015-05-11 15:06:30 -0700

    switch to version 0.6.0 for new package format

--- astformatter/__init__.py
+++ astformatter/__init__.py
@@ -83,7 +83,7 @@ class ASTFormatter(ast.NodeVisitor):
     .. |copy| unicode:: 0xA9 .. copyright sign
     '''
 
-    __version__ = '0.5.1'
+    __version__ = '0.6.0'
 
     def __init__(self):
         """Return a new ASTFormatter object."""
